9e76a4cc7c576e3cab31e037eee6ba9eb28922b3
austin84 <austinveatch84@gmail.com>
2020-05-07 12:43:19 -0700

    charts added, and mostly styled

--- js/script.js
+++ js/script.js
@@ -16,34 +16,58 @@ var tlg = document.getElementById("traffic-graph").getContext("2d");
 var trafficLineGraph = new Chart(tlg, {
   type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: [
+      "16-22",
+      "23-29",
+      "30-5",
+      "6-12",
+      "13-19",
+      "20-26",
+      "27-3",
+      "4-10",
+      "11-17",
+      "18-24",
+      "25-31",
+    ],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+        data: [
+          0,
+          750,
+          1250,
+          1000,
+          1500,
+          2000,
+          1500,
+          1750,
+          1250,
+          1750,
+          2250,
+          1750,
+          2250,
         ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+
+        pointStyle: "triangle",
+        pointBackgroundColor: ["rgb(255, 255, 255)"],
+        pointBorderColor: ["#7377BF"],
+        pointRadius: 6,
+        backgroundColor: ["rgba(116, 119, 191, .3)"],
+        borderColor: ["#7377BF"],
         borderWidth: 1,
+        lineTension: 0,
       },
     ],
   },
   options: {
+    aspectRatio: 2.5,
+    animation: {
+      duration: 0,
+    },
     responsive: true,
     scales: {
+      gridLines: {
+        offsetGridLines: true,
+      },
       yAxes: [
         {
           ticks: {
@@ -51,6 +75,9 @@ var trafficLineGraph = new Chart(tlg, {
           },
         },
       ],
+    },
+    legend: {
+      display: false,
     },
   },
 });
@@ -62,32 +89,20 @@ var dbc = document.getElementById("daily-graph").getContext("2d");
 var dailyBarChart = new Chart(dbc, {
   type: "bar",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: ["#7377BF"],
+        borderColor: ["#7377BF"],
         borderWidth: 1,
       },
     ],
   },
   options: {
+    legend: {
+      display: false,
+    },
     responsive: true,
     scales: {
       yAxes: [
@@ -108,36 +123,28 @@ var mdc = document.getElementById("mobile-graph").getContext("2d");
 var trafficLineGraph = new Chart(mdc, {
   type: "doughnut",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: ["Desktop", "Tablets", "Phones"],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
+        data: [2000, 550, 500],
+        backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"],
+        borderWidth: 0,
       },
     ],
   },
   options: {
+    legend: {
+      position: "right",
+      labels: {
+        boxWidth: 20,
+        fontStyle: "bold",
+      },
+    },
     responsive: true,
     scales: {
       yAxes: [
         {
+          display: false,
           ticks: {
             beginAtZero: true,
           },
